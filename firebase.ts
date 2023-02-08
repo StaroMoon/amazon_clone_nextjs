3f7490b74e76266e922ff5efc7a14380fd9f1524
StaroMoon <brightza123@gmail.com>
2023-02-08 19:49:02 +1100

    Add Nextauth for Google signin and Firebase

---
+++ firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDoQsBsoCY7aVV8VqjE62Ld_iiKnP7Od58",
+  authDomain: "clone-16d6b.firebaseapp.com",
+  projectId: "clone-16d6b",
+  storageBucket: "clone-16d6b.appspot.com",
+  messagingSenderId: "634833158213",
+  appId: "1:634833158213:web:150a56c72d4f74ef7e232c",
+};
+
+const app = initializeApp(firebaseConfig);
