b40c6182bfdee633c9a304e41298863b0ad93321
StaroMoon <brightza123@gmail.com>
2023-02-08 17:02:57 +1100

    Create a header and styled it

---
+++ src/components/Header.tsx
@@ -0,0 +1,73 @@
+import Image from "next/image";
+import {
+  AiOutlineMenu,
+  AiOutlineSearch,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
+
+const Header = () => {
+  return (
+    <header>
+      {/* Top nav */}
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+            alt="hi"
+            className="object-contain"
+          />
+        </div>
+        {/* Search */}
+        <div className="hidden sm:flex items-center h-10 rounded-md  flex-grow  cursor-pointer bg-yellow-400 hover:bg-yellow-500 ml-4">
+          <input
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+            type="text"
+          />
+          <AiOutlineSearch className="h-12 w-12 p-4" />
+        </div>
+
+        {/* Right */}
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className="link">
+            <p>Hello John Na</p>
+            <p className="font-extrabold md:text-sm">Account & Lists</p>
+          </div>
+          <div className="link">
+            <p>Returns</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
+          </div>
+          <div className="relative link flex items-center">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+              0
+            </span>
+            <AiOutlineShoppingCart className="h-10 w-10" />
+            <p className="hidden md:inline font-extrabold md:text-sm mt-2">
+              Basket
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Bottom nav */}
+      <div className="flex items-center bg-amazon_blue-light text-white text-sm space-x-3 p-2 pl-6">
+        <p className="link flex items-center">
+          <AiOutlineMenu className="h-6 mr-1 w-6" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deals</p>
+        <p className="link hidden lg:inline-flex">Electronics</p>
+        <p className="link hidden lg:inline-flex">Food & Grocery</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health & Personal Care</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
