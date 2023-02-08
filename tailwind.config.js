c3e654b403363076436ab5604f4a8a10ae68a6d5
StaroMoon <brightza123@gmail.com>
2023-02-08 15:29:09 +1100

    initialize files for project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        amazon_blue: {
+          light: "#232F3E",
+          DEFAULT: "#131921",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
