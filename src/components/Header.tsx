17a89e1148b7174895106dedb52a01f478bb97e7
StaroMoon <brightza123@gmail.com>
2023-02-12 17:27:45 +1100

    fix error in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -76,7 +76,7 @@ const Header = () => {
         </p>
         <p className="link">Prime Video</p>
         <p className="link">Amazon Business</p>
-        <p className="link">Today's Deals</p>
+        <p className="link">Today Deals</p>
         <p className="link hidden lg:inline-flex">Electronics</p>
         <p className="link hidden lg:inline-flex">Food & Grocery</p>
         <p className="link hidden lg:inline-flex">Prime</p>
