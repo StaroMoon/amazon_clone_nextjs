0aada6d4fe04c376c38e45cd6920a0f792afbf4b
StaroMoon <brightza123@gmail.com>
2023-02-12 18:50:42 +1100

    Add Stripe at Checkout

---
+++ src/pages/api/create-checkout-session.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from "next";
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { items, email } = req.body;
+  console.log(req.body);
+  // Transform our data into format that Stripe accepted
+  const transformedItems = items.map((item: any) => ({
+    quantity: 1,
+    price_data: {
+      currency: "aud",
+      unit_amount: item.price * 100,
+      product_data: {
+        name: item.title,
+        images: [item.image],
+        description: item.description,
+      },
+    },
+  }));
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    shipping_options: [{ shipping_rate: "shr_1Maa4eIoR1cIbDiSHDDbGYcG" }],
+    line_items: transformedItems,
+    shipping_address_collection: {
+      allowed_countries: ["AU", "TH"],
+    },
+    mode: "payment",
+    success_url: `${process.env.HOST}/success`,
+    cancel_url: `${process.env.HOST}/checkout`,
+    metadata: {
+      email,
+      images: JSON.stringify(items.map((item: any) => item.image)),
+    },
+  });
+
+  res.status(200).json({ id: session.id });
+};
--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -4,11 +4,34 @@ import { useSelector } from "react-redux";
 import { selectItems, selectTotal } from "@/slices/basketSlice";
 import CheckoutProduct from "@/components/CheckoutProduct";
 import { useSession } from "next-auth/react";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+const stripePromise = loadStripe(process.env.stripe_public_key as string);
 
 const Checkout = () => {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
   const { data: session } = useSession();
+  console.log(session);
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+
+    // Call the backend to create a checkout session...
+    const checkoutSession = await axios.post("/api/create-checkout-session", {
+      items,
+      email: session?.user?.email,
+    });
+
+    // Redirect user/customer to Stripe Checkout
+    const result = await stripe?.redirectToCheckout({
+      sessionId: checkoutSession.data.id,
+    });
+
+    if (result?.error) {
+      alert(result?.error.message);
+    }
+  };
+
   return (
     <div className="bg-gray-100">
       <Header />
@@ -55,6 +78,8 @@ const Checkout = () => {
               </h2>
 
               <button
+                role="link"
+                onClick={createCheckoutSession}
                 disabled={!session}
                 className={`button mt-2 ${
                   !session &&
