beebe8a5d1d1f562951a78677905027ec370a152
StaroMoon <brightza123@gmail.com>
2023-02-14 15:04:50 +1100

    Fix firebase private key error in webhook

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
   images: {
     domains: ["links.papareact.com", "fakestoreapi.com"],
   },
-  appDir: false,
 };
 
 module.exports = nextConfig;
--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -3,10 +3,23 @@ import { buffer } from "micro";
 import * as admin from "firebase-admin";
 
 // Secure a connection to FIREBASE from the backend
-const serviceAccount = require("../../../firebase-permission.json");
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT,
+};
 
 const app = !admin.apps.length
-  ? admin.initializeApp({ credential: admin.credential.cert(serviceAccount) })
+  ? admin.initializeApp({
+      credential: admin.credential.cert(JSON.stringify(serviceAccount)),
+    })
   : admin.app();
 
 // Establish connection to Stripe
